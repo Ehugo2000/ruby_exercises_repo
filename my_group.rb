899c4d1ffc760ac2bd0cdd280e031dcacf04c119
ehugo <mihaivos@gmail.com>
2020-10-07 11:57:30 +0200

    Excercise Person_1 etc

--- my_group.rb
+++ my_group.rb
@@ -1,3 +1,11 @@
-person_1 = {name: "Anna", gender: female, age: 10}
-person_2 = {name: "Erik", gender: male, age: 20}
-person_3 = {name: "Per", gender: male, age: 30}
+my_group = []
+
+person_1 = {name: "Anna", gender: "female", age: 10}
+person_2 = {name: "Erik", gender: "male", age: 20}
+person_3 = {name: "Per", gender: "male", age: 30}
+
+my_group.push person_1, person_2, person_3
+
+my_group.each do |person|
+puts "This person's name is #{person[:name]} and is a #{person[:gender]} at age #{person[:age]}!"
+end
